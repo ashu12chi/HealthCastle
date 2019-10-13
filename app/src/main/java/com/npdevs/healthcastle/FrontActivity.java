942ddb840e30025a87ebe6d33d9e1a721cdf1b10
Nishchal Siddharth Pandey <nisiddharth@gmail.com>
2019-10-13 09:43:25 +0530

    Fixed bugs, added family notifications for depression

--- app/src/main/java/com/npdevs/healthcastle/FrontActivity.java
+++ app/src/main/java/com/npdevs/healthcastle/FrontActivity.java
@@ -91,6 +91,8 @@ public class FrontActivity extends AppCompatActivity implements SensorEventListe
 	TensorFlowClassifier classifier;
 	static final int PIXEL_WIDTH = 48;
 
+	private boolean FIRST_TIME=true;
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 
@@ -562,7 +564,17 @@ public class FrontActivity extends AppCompatActivity implements SensorEventListe
 						String heart = member.getBloodpressure().get(member.getBloodpressure().size() - 1);
 						systole = Integer.parseInt(heart.substring(heart.indexOf('-') + 1));
 					}
-					if(sugar>120 || sugar<55 || systole<90 || systole>180) {
+					int depcount=0;
+					ArrayList<String> emo=member.getEmotions();
+					if(emo.size()>10) {
+						emo.remove(0);
+						for(String s : emo) {
+							if(s.equalsIgnoreCase("fear") || s.equalsIgnoreCase("angry")) {
+								depcount++;
+							}
+						}
+					}
+					if(sugar>120 || sugar<55 || systole<90 || systole>180 || depcount>=8) {
 						createNotificationChannel();
 						notification(family.get(i),member);
 					}
@@ -698,71 +710,73 @@ public class FrontActivity extends AppCompatActivity implements SensorEventListe
 
 	}
 
-	private void detectEmotion(){
+	private void detectEmotion() {
+		if(FIRST_TIME) {
 
-		Bitmap image=((BitmapDrawable)iv_image.getDrawable()).getBitmap();
-		Bitmap grayImage = toGrayscale(image);
-		Bitmap resizedImage=getResizedBitmap(grayImage,48,48);
-		int pixelarray[];
+			Bitmap image = ((BitmapDrawable) iv_image.getDrawable()).getBitmap();
+			Bitmap grayImage = toGrayscale(image);
+			Bitmap resizedImage = getResizedBitmap(grayImage, 48, 48);
+			int pixelarray[];
 
-		//Initialize the intArray with the same size as the number of pixels on the image
-		pixelarray = new int[resizedImage.getWidth()*resizedImage.getHeight()];
+			//Initialize the intArray with the same size as the number of pixels on the image
+			pixelarray = new int[resizedImage.getWidth() * resizedImage.getHeight()];
 
-		//copy pixel data from the Bitmap into the 'intArray' array
-		resizedImage.getPixels(pixelarray, 0, resizedImage.getWidth(), 0, 0, resizedImage.getWidth(), resizedImage.getHeight());
+			//copy pixel data from the Bitmap into the 'intArray' array
+			resizedImage.getPixels(pixelarray, 0, resizedImage.getWidth(), 0, 0, resizedImage.getWidth(), resizedImage.getHeight());
 
 
-		float normalized_pixels [] = new float[pixelarray.length];
-		for (int i=0; i < pixelarray.length; i++) {
-			// 0 for white and 255 for black
-			int pix = pixelarray[i];
-			int b = pix & 0xff;
-			//  normalized_pixels[i] = (float)((0xff - b)/255.0);
-			// normalized_pixels[i] = (float)(b/255.0);
-			normalized_pixels[i] = (float)(b);
+			float normalized_pixels[] = new float[pixelarray.length];
+			for (int i = 0; i < pixelarray.length; i++) {
+				// 0 for white and 255 for black
+				int pix = pixelarray[i];
+				int b = pix & 0xff;
+				//  normalized_pixels[i] = (float)((0xff - b)/255.0);
+				// normalized_pixels[i] = (float)(b/255.0);
+				normalized_pixels[i] = (float) (b);
 
-		}
-		System.out.println(normalized_pixels);
-		Log.d("pixel_values",String.valueOf(normalized_pixels));
-		String text=null;
-
-		try{
-			final Classification res = classifier.recognize(normalized_pixels);
-			//if it can't classify, output a question mark
-			if (res.getLabel() == null) {
-				text = "Status: "+ ": ?\n";
-			} else {
-				//else output its name
-				text = String.format("%s: %s, %f\n", "Status: ", res.getLabel(),
-						res.getConf());
-				final DatabaseReference myRef=FirebaseDatabase.getInstance().getReference("users/"+MOB_NUMBER);
-				myRef.addListenerForSingleValueEvent(new ValueEventListener() {
-					@Override
-					public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-						Users user=dataSnapshot.getValue(Users.class);
-						assert user != null;
-						ArrayList<String> emo=user.getEmotions();
-						emo.add(res.getLabel());
-						user.setEmotions(emo);
-						myRef.setValue(user);
-					}
-
-					@Override
-					public void onCancelled(@NonNull DatabaseError databaseError) {
-
-					}
-				});
 			}
-		}
-		catch (Exception e){
-			System.out.print("Exception:"+e.toString());
+			System.out.println(normalized_pixels);
+			Log.d("pixel_values", String.valueOf(normalized_pixels));
+			String text = null;
 
-		}
+			try {
+				final Classification res = classifier.recognize(normalized_pixels);
+				//if it can't classify, output a question mark
+				if (res.getLabel() == null) {
+					text = "Status: " + ": ?\n";
+				} else {
+					//else output its name
+					text = String.format("%s: %s, %f\n", "Status: ", res.getLabel(),
+							res.getConf());
+					final DatabaseReference myRef = FirebaseDatabase.getInstance().getReference("users/" + MOB_NUMBER);
+					myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+						@Override
+						public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+							Users user = dataSnapshot.getValue(Users.class);
+							assert user != null;
+							ArrayList<String> emo = user.getEmotions();
+							emo.add(res.getLabel());
+							user.setEmotions(emo);
+							myRef.setValue(user);
+						}
 
-		Toast.makeText(FrontActivity.this,text,Toast.LENGTH_LONG).show();
+						@Override
+						public void onCancelled(@NonNull DatabaseError databaseError) {
+
+						}
+					});
+				}
+			} catch (Exception e) {
+				System.out.print("Exception:" + e.toString());
+
+			}
+
+			Toast.makeText(FrontActivity.this, text, Toast.LENGTH_LONG).show();
+			FIRST_TIME=false;
+		}
 	}
 
-	public Bitmap toGrayscale(Bitmap bmpOriginal)
+	private Bitmap toGrayscale(Bitmap bmpOriginal)
 	{
 		int width, height;
 		height = bmpOriginal.getHeight();
@@ -779,7 +793,7 @@ public class FrontActivity extends AppCompatActivity implements SensorEventListe
 		return bmpGrayscale;
 	}
 
-	public Bitmap getResizedBitmap(Bitmap image, int bitmapWidth, int bitmapHeight) {
+	private Bitmap getResizedBitmap(Bitmap image, int bitmapWidth, int bitmapHeight) {
 		return Bitmap.createScaledBitmap(image, bitmapWidth, bitmapHeight, true);
 	}
 }
